2faf906181f79d232591403f5947316625431f18
imsamimalik <salaarwali.malik@GMAIL.com>
2021-01-11 23:29:57 +0500

    responsive header added

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { NavLink } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Hidden from '@material-ui/core/Hidden';
+import SideDrawer from "./SideDrawer"
+import NavLinks from './NavLinks';
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,20 +24,36 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const flex = {
+	"width": "60%",
+	"display":"flex",
+	"justify-content":"center",
+	"flex-direction":"row",
+}
+const toolbarStyle = {
+    "width": "100%",
+    "display": "flex",
+	"justify-content": "space-between",
+}
+
 export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+      <AppBar position="static" sty>
+        <Toolbar style={toolbarStyle}>
+		<Hidden mdUp>
+		<SideDrawer />
+</Hidden>
+<div style={flex}>
+<Hidden smDown>
+<NavLinks />
+</Hidden>
+</div>
+	
+          
+<Box component="span" m={2}><Button color="inherit"><AccountCircle /></Button></Box>
         </Toolbar>
       </AppBar>
     </div>
