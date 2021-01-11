f7b69343822ecb93014245b524f528d19ff51e8e
meeran <muhammadmeeran2003@gmail.com>
2021-01-11 20:46:51 +0500

    Started the Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+ 
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+                <div style={{justifyContent : "center",display : "flex",flex : 1,alignItems:"center"}}>
+                    <h1>Hello World</h1>
+                </div>
+            </>
+        )
+    }
+}
+
+export default App;
