b57314395694ef793cdbcbb6135db2d5385f900b
meeran <muhammadmeeran2003@gmail.com>
2021-01-13 20:36:00 +0500

    Added axios

---
+++ src/APIS.js
@@ -0,0 +1,32 @@
+export const SESSION = '/session';
+export const SERVER = '/server';
+export const DEVICES = '/devices';
+export const GROUPS = '/groups';
+export const USERS = '/users';
+export const PERMISSIONS = '/permissions';
+export const POSITIONS = '/positions';
+export const EVENTS = '/events';
+
+// Reports
+export const REPORTS_ROUTE = '/reports/route';
+export const REPORT_EVENTS = '/reports/events';
+export const REPORT_SUMMARY = '/reports/summary';
+export const REPORT_TRIPS = '/reports/trips';
+export const REPORT_STOPS = '/reports/stops';
+
+export const NOTIFICATIONS = '/notifications';
+export const NOTIFICATIONS_TYPES = '/notifications/types';
+
+export const GEOFENCES = '/geofences';
+
+export const COMMANDS = '/commands';
+
+// Attributes
+export const ATTRIBUTES_COMPUTED = '/attributes/computed';
+export const ATTRIBUTES_COMPUTED = '/attributes/computed';
+
+export const DRIVERS = '/drivers';
+export const MAINTENANCE = '/maintenance';
+export const CALENDARS = '/calendars';
+export const STATISTICS = '/statistics';
+
---
+++ src/Connector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const instance = axios.create({
+    baseURL: 'https://167.86.71.215:8082/api'
+  });
+
+export default instance;
